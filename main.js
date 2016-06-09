013adcfb4df46a00d2aaf8028ee63a7e4cf01d45
Andreas N <andy@pixelde.su>
2016-06-09 20:52:33 +0200

    move IPC channels into own file

--- main.js
+++ main.js
@@ -69,50 +69,7 @@ function createWindow() {
       });
     });
 
-    // Let's wait for sent tweets..
-    ipcMain.on('linnun:send', function(e, tweet) {
-        // ..and then post them to Twitter..
-        if (tweet.id !== undefined) {
-            // ..with an ID attached (as reply)
-            twitter.post('statuses/update', { status: tweet.text, in_reply_to_status_id: tweet.id }, function(err, data, response) {
-                // do nothing!
-            })
-        } else {
-            // ..with no ID attached (as puretweet)
-            twitter.post('statuses/update', { status: tweet.text }, function(err, data, response) {
-            // do nothing!
-            })
-        }
-
-    });
-
-    // Let's wait for retweets..
-    ipcMain.on('linnun:retweet', function(e, tweet) {
-        // ..and then post them to Twitter
-        if (tweet.type == "retweet") {
-            twitter.post('statuses/retweet/:id', { id: tweet.id }, function (err, data, response) {
-                // do nothing!
-            })
-        } else if (tweet.type == "unretweet") {
-            twitter.post('statuses/unretweet/:id', { id: tweet.id }, function (err, data, response) {
-                // do nothing!
-            })
-        }
-    })
-
-    // Let's wait for favorites..
-    ipcMain.on('linnun:favorite', function(e, tweet) {
-        // ..and then post them to Twitter
-        if (tweet.type == "favorite") {
-            twitter.post('favorites/create', { id: tweet.id }, function (err, data, response) {
-                // do nothing!
-            })
-        } else if (tweet.type == "unfavorite") {
-            twitter.post('favorites/destroy', { id: tweet.id }, function (err, data, response) {
-                // do nothing!
-            })
-        }
-    })
+    require('./src/twitter/actions');
 }
 
 function createAuthWindow() {
