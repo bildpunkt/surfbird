2d7d655fd8ffff87f39c1e232c3a0cf195e38668
Andreas Nedbal <andreas.nedbal@in2code.de>
2016-07-13 10:52:22 +0200

    append file protocol to open folder in correct manner

--- src/themes.js
+++ src/themes.js
@@ -17,6 +17,6 @@ module.exports = function(app, mainWindow) {
     })
 
     ipcMain.on("surfbird:open:themes", function(e) {
-        shell.openExternal(app.getPath("documents"))
+        shell.openExternal("file:///" + app.getPath("documents"))
     })
 }
