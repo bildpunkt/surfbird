ce36c9e739c5967cde6f1c570803534868ce56c4
Andreas N <andy@pixelde.su>
2016-08-27 02:22:54 +0200

    add lightboxes to interaction images

--- app/_js/vue/interaction.vue
+++ app/_js/vue/interaction.vue
@@ -17,7 +17,7 @@
         </div>  
         <div class="tweet-text">{{{ content.target_object.text_html }}}</div>
         <div class="tweet-media-wrapper media-{{ content.target_object.extended_entities.media.length }}" v-if="content.target_object.extended_entities !== undefined">
-          <div v-for="media in content.target_object.extended_entities.media" class="media-image" v-bind:style="{ backgroundImage: 'url(' + media.media_url_https + ')' }"></div>
+          <a v-for="media in content.target_object.extended_entities.media" class="media-image" v-bind:href="media.media_url_https" v-bind:data-lightbox="content.target_object.id_str + content.created_at" v-bind:title="content.target_object.text" v-bind:style="{ backgroundImage: 'url(' + media.media_url_https + ')' }"></a>
         </div>
       </div>  
     </div>
