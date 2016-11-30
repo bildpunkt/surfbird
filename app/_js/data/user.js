188ecc1525a41b96627a7cf371e3baa1c977b505
Andreas N <git@pixelde.su>
2016-11-30 21:06:29 +0100

    *poof*

--- app/_js/data/user.js
+++
@@ -1,8 +0,0 @@
-const ipcRenderer = require('electron').ipcRenderer
-
-module.exports = function (vm, app) {
-  ipcRenderer.on('surfbird:get:user', function (e, user) {
-    app.user = user
-    vm.$set('user', user)
-  })
-}
