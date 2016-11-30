fdb76c51ff8d7eac2b748936f778c32a7872fdf7
Andreas N <git@pixelde.su>
2016-11-30 21:57:58 +0100

    Add missing configureStore for development
    Wow, this is missing from the boilerplate, which actually causes it to
    fail? Not cool, not cool.

--- app/store/configureStore.development.js
+++ app/store/configureStore.development.js
@@ -4,3 +4,14 @@ import { hashHistory } from 'react-router';
 import { routerMiddleware, push } from 'react-router-redux';
 import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
+
+export default function configureStore(initialState) {
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(
+      thunk,
+      //createLogger(),
+    ),
+  );
+}
