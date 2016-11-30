b7f3d56d59d7405799da78c1185c8b02159a7612
Andreas N <git@pixelde.su>
2016-11-30 21:26:48 +0100

    Set up electron-react-boilerplate

---
+++ app/components/Home.js
@@ -0,0 +1,16 @@
+// @flow
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import styles from './Home.css';
+
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div className={styles.container}>
+        </div>
+      </div>
+    );
+  }
+}
