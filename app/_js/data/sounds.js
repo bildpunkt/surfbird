c14e16f89a4a320c1bb327953a34336772e75ca9
Andreas N <git@pixelde.su>
2017-02-04 18:25:41 +0100

    *poof*

--- app/_js/data/sounds.js
+++
@@ -1,9 +0,0 @@
-const ipcRenderer = require('electron').ipcRenderer
-
-module.exports = function (app) {
-  ipcRenderer.on('surfbird:get:sounds', function (e, sound) {
-    if (!(JSON.stringify(app.sounds).indexOf(JSON.stringify(sound)) > 0)) {
-      app.sounds.push(sound)
-    }
-  })
-}
