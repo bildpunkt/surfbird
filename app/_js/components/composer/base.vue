6ba11d76b9f16b13b94024daef15e39f2dd1dee0
Andreas N <git@pixelde.su>
2017-02-11 16:48:20 +0100

    feat(application): Add composer
    This commit adds the composer component and design

---
+++ app/_js/components/composer/base.vue
@@ -0,0 +1,32 @@
+<style>
+</style>
+
+<template>
+  <div class="c-composer">
+    <textarea class="c-composer__input" placeholder="What's happening, username?">
+    </textarea>
+    <div class="c-composer__footer">
+      <ul class="c-composer__actions">
+        <li class="c-composer__action c-composer__action-message">
+          
+        </li>
+        <li class="c-composer__action c-composer__action-media">
+          
+        </li>
+      </ul>
+      <div class="c-composer__submit">
+        <span class="c-composer__character-count">
+          140
+        </span>
+        <button class="c-composer__send">
+          Send
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
