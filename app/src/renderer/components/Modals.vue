64223ae4bb8411d19f3cafcf5ccaec7e90e9be57
Andreas N <git@pixelde.su>
2017-07-15 03:39:29 +0200

    feat(application): dynamic column types and column modal
    This commit adds the base structure for dynamic column types and the
    modal for column creation.

--- app/src/renderer/components/Modals.vue
+++ app/src/renderer/components/Modals.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="l-modals">
     <authentication-modal></authentication-modal>
+    <column-modal></column-modal>
   </div>  
 </template>
 
 <script>
 import AuthenticationModal from './Modal/Authentication'
+import ColumnModal from './Modal/Column'
 
 export default {
   name: 'modals',
   components: {
-    AuthenticationModal
+    AuthenticationModal,
+    ColumnModal
   }
 }
 </script>
