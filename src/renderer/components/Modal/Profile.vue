c939f18fb92a77b1dc0cb3bde3c417c8a661715d
Priefy <peachy-lovly-mom@hotmail.com>
2017-12-02 00:33:02 +0700

    feat: Profile Creation Dialogue
    Create a 'Profile.vue'
    
    Closes #55

---
+++ src/renderer/components/Modal/Profile.vue
@@ -0,0 +1,42 @@
+<template>
+    <modal name="profile-modal" @before-open="beforeOpen">
+        <h1>Create a profile</h1>
+
+        <p>
+            <label for="column-name"> Profile Name</label>
+            <input type="text" for="column-name" v-model="profile_name"/>
+        </p>
+
+        <p>
+            <button @click="createColumn">Create profile</button>
+        </p>
+    </modal>
+</template>
+
+<script>
+    export default {
+        name: 'profile-modal',
+        data: function () {
+            return {
+                profile_name: ''
+            }
+        },
+        methods: {
+            beforeOpen () {
+                this.profile_name = ''
+            },
+            createColumn () {
+                let payload = {
+                    name: this.profile_name,
+                }
+
+                this.$store.dispatch('addProfile', payload)
+                this.$modal.hide('profile-modal')
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
